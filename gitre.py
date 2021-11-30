4f64156c4608a3cc6948f960d705e9a3c676cbcb
Tanuló 301-12 <301-12@KKSZKI.LOCAL>
2021-11-30 18:22:57 +0100

    Első feladat

--- gitre.py
+++ gitre.py
@@ -1,2 +1,10 @@
-print("Git beállítása")
-print("Ez a folytatás!")
+#1. Írjon programot, mely kiírja a képernyőre a 1-10 ig a számok reciprokát!
+
+for szam in range (1,11,1):
+    reciprok=1/szam
+    print(reciprok)
+
+#2. írj programot, mely beolvassa a hatvány alapját és a kitevőt, és kiírja a hatványértéket!
+
+#3. Írj programot, ami csak pozitív számot hajlandó beolvasni.
+
