fe999e7f93efcbb48cf2c117b0b09270c2b2a3e6
doviki <dobosv@kkszki.hu>
2021-12-01 22:09:44 +0100

    initial commit with gitre.py file

--- gitre.py
+++ gitre.py
@@ -14,11 +14,22 @@ print(hatvanyertek)
 #3. Írj programot, ami csak pozitív számot hajlandó beolvasni.
 
 a=int(input("Adj meg egy számot: "))
-if a>0:
+if a>=0:
     print(a)
 else:
     print("A szám negatív")
     
 
+#4. írj programot, mely bekér két számot és eldönti mennyi a távolságuk a számegyenesen!
 
+a=int(input("Adj meg egy számot: "))
+b=int(input("Adj meg még egy számot: "))
+x=a-b
+
+if a<0:
+    print("{0} két szám távolsága a számegyensen".format(-(x)))
+else:       
+    print("{0} két szám távolsága a számegyensen".format(x))
+
+#5. írj programot, mely addig kér számokat a billentyűzetről amig összegük kisebb mint 100!
 
